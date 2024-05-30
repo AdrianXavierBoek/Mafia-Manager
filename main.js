64263bbf8f00f746081f4f08b08ab0ddb098f816
Adrian Boek <adrian.x.boek@gmail.com>
2024-05-31 00:20:06 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,22 @@ const { app, BrowserWindow, Tray, ipcMain, ipcRenderer } = require("electron");
 const path = require("path");
 const fs = require("fs");
 
+// Create data directory
+const directoryPath = path.join(__dirname, 'data');
+
+if (!fs.existsSync(directoryPath)){
+  fs.mkdirSync(directoryPath);
+}
+
+// Create data file
+const filePath = path.join(directoryPath, 'data.json');
+
+fs.writeFile(filePath, (err) => {
+  if (err) throw err;
+  console.log('data.json file created successfully!');
+});
+
+
 //Create main window
 let mainWindow;
 
